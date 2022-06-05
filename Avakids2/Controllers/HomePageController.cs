84ebabbc8df85270de6b3c790e2b8aeb28132b49
WhatTheDuck <41006973+xuandat282@users.noreply.github.com>
2022-06-06 00:19:34 +0700

    update ds sp

--- Avakids2/Controllers/HomePageController.cs
+++ Avakids2/Controllers/HomePageController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using Avakids2.Models;
 namespace Avakids2.Controllers
 {
     public class HomePageController : Controller
     {
+        private AVAKIDEntities2 db = new AVAKIDEntities2();
         // GET: HomePage
         public ActionResult Index()
         {
-            return View();
+            return View(db.SanPhams.ToList());
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
         }
     }
 }
